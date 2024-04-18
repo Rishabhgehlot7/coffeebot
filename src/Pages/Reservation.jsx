fab572f163c46cd3e77b3b5a50a65b152a3b2bd0
rishabh gehlot <rishabh3x@gmail.com>
2024-04-18 19:31:16 +0530

    it is resvestion commit

--- src/Pages/Reservation.jsx
+++ src/Pages/Reservation.jsx
@@ -8,6 +8,10 @@ const Reservation = () => {
             <section className=" w-screen bg-cover bg-center BgImage4 justify-center items-center flex pt-32">
                 <main className="bg-black/50 w-[90%] p-10 rounded-2xl mt-10 flex justify-evenly my-10 flex-col md:flex-row gap-3">
                     <form action="" className=" flex flex-col md:w-[40%] w-full">
+                        <div className=" py-5">
+                            <h4 className=" text-center text-xl text-white">Fill out the booking form</h4>
+                            <h2 className=" text-center text-5xl font-bold text-white">  Book a table</h2>
+                        </div>
                         <input type="text" className=" bg-transparent border rounded-lg w-full p-2 my-3 text-white outline-none" placeholder="Name" />
                         <input type="text" className=" bg-transparent border rounded-lg w-full p-2 my-3 text-white outline-none" placeholder="Phone" />
                         <input type="date" id="birthdaytime" name="birthdaytime" className=" bg-transparent border rounded-lg w-full p-2 my-3 text-white outline-none " />
