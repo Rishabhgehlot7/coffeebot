5740072380d8ed66b33065b3721139179d54063b
rishabh gehlot <rishabh3x@gmail.com>
2024-04-18 20:09:45 +0530

    "Added Link to Continue Shopping in Cart page"
    This commit adds a Link component from react-router-dom to the "Continue Shopping" text in the Cart page, allowing users to navigate back to the main shopping page

--- src/Pages/Cart.jsx
+++ src/Pages/Cart.jsx
@@ -1,4 +1,5 @@
 import React, { useContext, useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 import { CartContext } from '../CartContext/MainContext';
 import Footer from '../Components/Footer';
 import Header from '../Components/Header';
@@ -44,11 +45,11 @@ const Cart = () => {
                             )
                         })}
 
-                        <div href="#" className="flex font-semibold text-indigo-600 text-sm mt-10">
+                        <Link to={'/'} className="flex font-semibold text-indigo-600 text-sm mt-10">
 
                             <svg className="fill-current mr-2 text-indigo-600 w-4" viewBox="0 0 448 512"><path d="M134.059 296H436c6.627 0 12-5.373 12-12v-56c0-6.627-5.373-12-12-12H134.059v-46.059c0-21.382-25.851-32.09-40.971-16.971L7.029 239.029c-9.373 9.373-9.373 24.569 0 33.941l86.059 86.059c15.119 15.119 40.971 4.411 40.971-16.971V296z" /></svg>
                             Continue Shopping
-                        </div>
+                        </Link>
                     </div>
 
                     <div id="summary" className="w-full px-8 py-10">
